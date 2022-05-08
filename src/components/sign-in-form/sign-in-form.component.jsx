79ee4d5a99e2734d22969195479312ffa0222f0d
Ayokayzy <theayokayzy1@gmail.com>
2022-05-08 12:14:08 -0700

    created authentication page component and sign in form

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -4,7 +4,11 @@ import "./sign-in-form.styles.scss";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
-import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
+} from "../../utils/firebase/firebase.utils";
 
 const defaultFormFields = {
   email: "",
@@ -28,10 +32,26 @@ const SignInForm = () => {
     event.preventDefault();
 
     try {
-      const {user} = await signInAuthUserWithEmailAndPassword(email, password)
+      const { user } = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
 
       resetFormFields();
-    } catch (err) {}
+    } catch (err) {
+      switch (err.code) {
+        case "auth/wrong-password":
+          alert("incorrect password for email");
+          break;
+
+        case "auth/user-not-found":
+          alert("no user associated with this email");
+          break;
+
+        default:
+          console.log(err);
+      }
+    }
   };
 
   const handleChange = (event) => {
@@ -65,7 +85,7 @@ const SignInForm = () => {
         />
         <div className="buttons-container">
           <Button type="submit">Sign Up</Button>
-          <Button buttonType='google' onClick={signInWithGoogle}>
+          <Button buttonType="google" type="button" onClick={signInWithGoogle}>
             Sign Up
           </Button>
         </div>
--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -36,7 +36,7 @@ const SignUpForm = () => {
         email,
         password
       );
-      const userDocRef = await createUserDocumentFromAuth(user, {
+      await createUserDocumentFromAuth(user, {
         displayName,
       });
     } catch (err) {
